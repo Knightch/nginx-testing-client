4b8c38377043f2ad2baac84d60a872dd2e294a06
knightch <amankumarpandey2397@gmail.com>
2024-02-27 10:55:25 +0530

    initial setup client

---
+++ src/test/java/nginxTraficTest/com/client/ClientApplicationTests.java
@@ -0,0 +1,13 @@
+package nginxTraficTest.com.client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
